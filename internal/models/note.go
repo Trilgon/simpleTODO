a6540f092e3afe4ff058083f236c883adf998515
Trilgon <say243.ko@gmail.com>
2022-10-15 09:27:10 +0300

    Add validate tag to note model and add user model

--- internal/models/note.go
+++ internal/models/note.go
@@ -1,11 +1,11 @@
 package models
 
 type Note struct {
-	Id        int     `json:"id" db:"id"`
-	Email     string  `json:"email" db:"email"`
-	Title     string  `json:"title" db:"title"`
+	Id        int     `json:"id" db:"id" validate:"required, gt=0"`
+	Email     string  `json:"email" db:"email" validate:"required"`
+	Title     string  `json:"title" db:"title" validate:"required"`
 	Text      *string `json:"text" db:"text"`
-	StartDate string  `json:"start_date" db:"start_date"`
+	StartDate string  `json:"start_date" db:"start_date" validate:"required"`
 	EndDate   *string `json:"end_date" db:"end_date"`
-	IsDone    bool    `json:"is_done" db:"is_done"`
+	IsDone    bool    `json:"is_done" db:"is_done" validate:"required"`
 }
